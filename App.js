aca7a9a53e3e3fec2912c6c9b5d5645e4d0ed356
EmreKaya0860 <h.emrekaya2207@gmail.com>
2023-11-24 13:15:32 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { store } from "./src/app/store";
+import BecomeCustomerScreen from "./src/features/becamoCustomer/BecomeCustomerScreen";
+
+import { Provider } from "react-redux";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <Provider store={store}>
+        <BecomeCustomerScreen />
+      </Provider>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
